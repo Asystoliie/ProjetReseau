2c7fd791d9b1566ef7b07400303ba1ebfe411361
cdurand03 <cdurand03@u16.fdsetu.infra.umontpellier.fr>
2018-11-20 15:34:10 +0100

    Ajout des fonctions reception_tcp et envoi_tcp

--- src/function.c
+++ src/function.c
@@ -63,3 +63,48 @@ void flag2(InfoClient* infoClient, SharedStruct* sharedStruct, char* pseudo){
 	}
 }
 
+int reception_tcp(int destinataire, void * msg, int taille_msg)
+{
+	int nb_octet_recu=0;
+	int rez;
+	while(nb_octet_recu!=taille_msg)
+	{
+		rez= recv(destinataire, msg+nb_octet_recu, taille_msg-nb_octet_recu, 0);
+		if(rez<0)
+		{
+			printf("PROBLEME RECEPTION FONCTION\n");
+			return 1;
+		}
+		else if(rez==0)
+		{
+			printf("Socket ferme\n");
+			return 2;
+		}
+		nb_octet_recu= nb_octet_recu+rez;
+	}
+	return 0;
+}
+
+int envoi_tcp(int destinataire,const void * msg, int taille_msg)
+{
+	int nb_octet_envoi=0;
+	int rez;
+	while(nb_octet_envoi!=taille_msg)
+	{
+		rez = send(destinataire, msg+nb_octet_envoi, taille_msg-nb_octet_envoi, 0);
+		if(rez<0)
+		{
+			printf("PROBLEME ENVOI FONCTION\n");
+			exit(1);
+		}
+		else if(rez==0)
+		{
+			printf("Socket ferme\n");
+			return 2;
+		}
+
+		nb_octet_envoi=nb_octet_envoi+rez;
+	}
+	return 0;
+}
+
